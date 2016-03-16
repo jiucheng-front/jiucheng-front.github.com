86413192706eb11f67a38d08d7f3a1d0bd52e915
wjf444128852 <444128852@qq.com>
2016-03-17 03:24:26 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -10,3 +10,65 @@ $(function(){
         $(".big_form").removeClass("selected");
     });
 });
+/**
+ * Created by jason on 2016/1/8.
+ */
+$(function(){
+    //点击登录按钮
+    // $(".login").click(function(){
+    //     $(".big_form").css({
+    //         display:"block"
+    //     })
+    // });
+    // $(".close").click(function(){
+    //     $(".big_form").css({
+    //         display:"none"
+    //     })
+    // });
+    $(".list").mouseover(function(){
+        $(this).find($(".will_show")).css({
+            display:"block"
+        })
+    });
+    $(".list").mouseout(function(){
+        $(this).find($(".will_show")).css({
+            display:"none"
+        })
+    });
+});
+$(function(){
+        $(".top1").delay(500).animate({
+            width: "100%"
+        }, 500);
+
+        $(".top2").delay(1000).animate({
+            height:"100%"
+        },500);
+
+        $(".top3").delay(1500).animate({
+            width:"100%",
+            marginLeft:"-100%"
+        },500);
+
+        $(".top4").delay(2000).animate({
+            height:"100%"
+        },500);
+});
+$(function(){
+    $(".top5").delay(500).animate({
+        width: "100%",
+        marginLeft:"-100%"
+    }, 500);
+
+    $(".top6").delay(1000).animate({
+        height:"100%"
+    },500);
+
+    $(".top7").delay(1500).animate({
+        width:"100%"
+    },500);
+
+    $(".top8").delay(2000).animate({
+        height:"100%"
+    },500);
+});
