42993342f297b3462e16e59b6b128ba6a26abf23
wjf444128852 <444128852@qq.com>
2016-03-16 23:43:10 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,12 +1,17 @@
 /**
- * Created by jason on 2016/1/8.
+ * Created by jason on 2016/3/16.
  */
 $(function(){
     //点击登录按钮
     $(".login").click(function(){
-        $(".big_form").show(500);
+        $(".big_form").css({
+            display:"block"
+        });
+        console.log("aaa");
     });
     $(".close").click(function(){
-        $(".big_form").hide(1000);
+        $(".big_form").css({
+            display:"none"
+        })
     });
 });
