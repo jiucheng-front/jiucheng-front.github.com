214fa930b13883d00be3d85d40dc04b88ab1481e
unknown <444128852@qq.com>
2016-08-17 17:50:48 +0800

    沈阳东城专家

---
+++ sumDemos/myweb/js/script.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function(){
+    $('.func li').mouseover(function(){
+    $(this).removeClass();	
+    $(this).find('.mast').slideDown("fast");
+    });
+    $('.func li').mouseleave(function(){
+     $(this).removeClass();
+    $(this).find('.mast').slideUp("fast");
+    });
+});
+
+$(document).ready(function  () {
+
+/**********************/
+    $('#show_banners img').lazyload({effect:"fadeIn"})
+/**********************/
+
+});
+
+$(document).ready(function  () {
+    jQuery(".fullSlide").slide({
+        titCell: ".hd ul",
+        mainCell: ".bd ul",
+        effect: "fold",
+        autoPlay: !0,
+        autoPage: !0,
+        trigger: "click",
+        startFun: function(l) {
+            var e = jQuery(".fullSlide .bd li").eq(l);
+            e.attr("_src") && e.css("background-image", e.attr("_src")).removeAttr("_src")
+        }
+    }),
+    $(function() {
+        $(".fullSlide").hover(function() {
+            $(this).find(".prev,.next").css("display", "block")
+        }
+        , function() {
+            $(this).find(".prev,.next").css("display", "none")
+        }
+        )
+    });
+});
