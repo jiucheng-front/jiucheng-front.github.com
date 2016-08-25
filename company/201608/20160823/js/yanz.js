a1559ad7362dd73d1d677f30f1905c291a8cc7ec
unknown <444128852@qq.com>
2016-08-25 09:16:46 +0800

    东城八月活动V2+0823JS固定

---
+++ company/201608/20160823/js/yanz.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+function check() {
+if (gh_form.name.value==""){alert("请填入您的姓名");gh_form.name.focus();return false}
+if (gh_form.tel.value==""){alert("请填入您的联系方式");gh_form.tel.focus();return false}
+if (gh_form.date.value=="" || gh_form.date.value=="点击选择就诊日期"){alert("请填入您的就诊日期");gh_form.date.focus();return false}
+return true;
+}
+//过滤空格
+function ignoreSpaces(string) {
+var temp = "";
+string = '' + string;
+splitstring = string.split(" ");
+for(i = 0; i < splitstring.length; i++)
+temp += splitstring[i];
+return temp;
+}
