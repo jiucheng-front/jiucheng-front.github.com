f2c41441a8917fba4e827d764d8036c5ec3e87a3
unknown <444128852@qq.com>
2016-09-24 10:30:07 +0800

    change0922

--- company/201609/0922/js/infinite_show.js
+++ company/201609/0922/js/infinite_show.js
@@ -18,8 +18,32 @@ var adv={
         timer=setTimeout(function(){
             adv.displayBlock();
         },3000);
+    },
+    //页面刚加载的3秒内该弹框不显示
+    index:0,
+    timerTwo:null,
+    fn:function(){
+        adv.timerTwo=setInterval(function(){
+            adv.index+=1;
+            console.log(adv.index);
+            if(adv.index>=3){
+                document.getElementById("middleBox").style.display="block";
+                adv.init();
+                clearInterval(adv.timerTwo);
+            }
+        },1000);
     }
 };
 window.onload=function(){
-    adv.init();
+    //var t=0;
+    //var timer=setInterval(function(){
+    //    t+=1;
+    //    console.log(t);
+    //    if(t>3){
+    //        document.getElementById("middleBox").style.display="block";
+    //        adv.init();
+    //        clearInterval(timer);
+    //    }
+    //},1000);
+    adv.fn();
 };
