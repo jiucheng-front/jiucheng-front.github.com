dfc5c414bec3dd01aa42f279a556977220e17766
unknown <444128852@qq.com>
2016-10-15 17:13:18 +0800

    add plug about jquery

---
+++ sumDemos/jQuery/jqPlug/js/jq-changeColor-plug3.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/10/14.
+ */
+(function($){
+    $.fn.changeColor3=function(colorStr,fontSize){
+        this.css("color",colorStr).css("fontSize",fontSize+"px");
+        return this;
+    }
+})(jQuery);
---
+++ sumDemos/jQuery/jqPlug/js/jq-changeColor-plug4.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/10/14.
+ */
+(function($){
+    $.fn.changeColor4=function(option){
+        this.css("color",option.colorStr).css("fontSize",option.fontSize+"px");
+        return this;
+    }
+})(jQuery);
