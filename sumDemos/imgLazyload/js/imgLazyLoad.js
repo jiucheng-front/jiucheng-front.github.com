33d0b28e3f480a22aa1e6f1c1f94203688c7d225
unknown <444128852@qq.com>
2016-11-28 11:36:45 +0800

    add img lazyLoading demo

--- sumDemos/imgLazyload/js/imgLazyLoad.js
+++ sumDemos/imgLazyload/js/imgLazyLoad.js
@@ -2,8 +2,8 @@
  * Created by Administrator on 2016/11/28.
  */
 !function(){
-    var num = document.getElementsByTagName('img').length;
-    var img = document.getElementsByTagName("img");
+    var imgs = document.getElementsByTagName("img");
+    var num = imgs.length;
     var n = 0; //存储图片加载到的位置，避免每次都从第一张图片开始遍历
     lazyload(); //页面载入完毕加载可是区域内的图片
     window.onscroll = lazyload;
@@ -11,9 +11,9 @@
         var seeHeight = document.documentElement.clientHeight; //可见区域高度
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //滚动条距离顶部高度
         for (var i = n; i < num; i++) {
-            if (img[i].offsetTop < seeHeight + scrollTop) {
-                if (img[i].getAttribute("src") == "") {
-                    img[i].src = img[i].getAttribute("data-src");
+            if (imgs[i].offsetTop < seeHeight + scrollTop) {
+                if (imgs[i].getAttribute("src") == "") {
+                    imgs[i].src = imgs[i].getAttribute("data-src");
                 }
                 n = i + 1;
             }
