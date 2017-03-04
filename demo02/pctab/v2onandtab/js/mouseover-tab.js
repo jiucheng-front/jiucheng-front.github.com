e75024705cc9020401410579e6ce444640822f57
jiucheng <444128852@qq.com>
2017-03-04 18:45:06 +0800

    add the tab and onmouse effect

---
+++ demo02/pctab/v2onandtab/js/mouseover-tab.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wangrq on 2017/3/2.
+ */
+//1定义构造函数
+function Overitem(option){
+    this.init(option);
+}
+Overitem.prototype={
+    init:function(option){
+        var _self=this;
+        _self.btn=document.getElementById(option.btn)||'';
+        _self.item=document.getElementById(option.item)||'';
+        _self.addclassname=option.addclass;
+        _self.btn.onmouseover=function(){
+            this.className=_self.addclassname;
+            _self.item.style.display='block';
+        };
+        _self.btn.onmouseout=function(){
+            _self.addclassname='';
+            _self.item.style.display='none';
+        };
+    }
+};
