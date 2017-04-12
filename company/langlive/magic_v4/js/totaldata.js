7ce99dcbc38a614a8ff0a664a6c9401c1a0b3fc3
wangjianfei <444128852@qq.com>
2017-04-12 19:25:35 +0800

    添加是否审核成功，审核中，审核未通过

---
+++ company/langlive/magic_v4/js/totaldata.js
@@ -0,0 +1,98 @@
+/*
+* @Author: wangjianfei
+* @Date:   2017-04-10 15:55:19
+* @Last Modified by:   wangjianfei
+* @Last Modified time: 2017-04-10 16:03:05
+*/
+
+'use strict';
+/*
+{
+    "ret_code": "0",
+    "ret_msg": "ok",
+    "data": {
+        "all_anchor_info": {
+            "1": [ //青铜相框
+                {
+                    "pfid": "1000002",   //主播id
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/c8f137446774494db18e41839c4a788f.jpg",  //主播头像
+                    "nickname": "缘由天定娃娃",  //主播昵称
+                    "sex": "1",  //主播性别
+                    "lvl": "57", //主播等级
+                    "live_id": null, //主播开播状态相关
+                    "status": "0",   //主播开播状态相关
+                    "follow_status": 0  //主播追踪状态相关
+                }
+            ],
+            "2": [], //白银相框
+            "3": [], //黄金相框
+            "4": []  //钻石相框
+        }
+    }
+}
+
+*/ 
+
+
+var totaldata={
+    "ret_code": "0",
+    "ret_msg": "ok",
+    "data": {
+        "all_anchor_info": {
+            "1": [
+                {
+                    "pfid": "1000002",
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/c8f137446774494db18e41839c4a788f.jpg",
+                    "nickname": "缘由天定娃娃",
+                    "sex": "1",
+                    "lvl": "57",
+                    "live_id": null,
+                    "status": "0"
+                },
+                {
+                    "pfid": "1024124",
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/ee45ce71acecbed01e2e19d97c8e53f109d0589b7d0652d51e276f839f92298c",
+                    "nickname": "嗨",
+                    "sex": "1",
+                    "lvl": "2",
+                    "live_id": null,
+                    "status": "0"
+                },
+                {
+                    "pfid": "1000037",
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/ff33eb72fe3940bdb267098975785e7446e7b4c4eae0b64f8bd75c8ffb6b24be",
+                    "nickname": "湉湉开心湉湉开心?",
+                    "sex": "1",
+                    "lvl": "186",
+                    "live_id": null,
+                    "status": "2"
+                },
+                {
+                    "pfid": "1000039",
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/25f0e2243ac9f515ed3be2fb1854c8b7db8e04b195a088962179cf5937ae25dd",
+                    "nickname": "哇呀唔唔無武呃呃😂哇咔",
+                    "sex": "2",
+                    "lvl": "163",
+                    "live_id": null,
+                    "status": "0"
+                }
+            ],
+            "2": [
+                {
+                    "pfid": "1000639",
+                    "headimg": "http://playback-langlive.ufile.ucloud.com.cn/d24e9f3e8dfc7193a969f22b528e2dc1.jpg",
+                    "nickname": "33444555",
+                    "sex": "1",
+                    "lvl": "33",
+                    "live_id": "10006391491804091",
+                    "status": "1",
+                    "live_key": "hblI2t",
+                    "stream_type": 0,
+                    "live_url": "rtmp://video.langlive.com/live/10006391491804091"
+                }
+            ],
+            "3": [],
+            "4": []
+        }
+    }
+}
