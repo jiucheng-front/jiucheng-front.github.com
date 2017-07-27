67b00c672c372b11b0fc621fad2c6308c571166a
wangjianfei <444128852@qq.com>
2017-07-27 11:14:51 +0800

    跟新最後版本

--- html/pirates/js/ranking.js
+++ html/pirates/js/ranking.js
@@ -59,8 +59,8 @@
   // 6、AJAX封装
   function nativeAjax(option,success,error){
     // 定义domain,方便环境切换
-    // var domain='https://' + window.location.host + '/';
-    var domain='http://' + window.location.host + '/';
+    var domain='https://' + window.location.host + '/';
+    // var domain='http://' + window.location.host + '/';
     var url=domain+option.urlStr;
     var type=option.ajaxType;
     var data=option.ajaxData;
@@ -96,9 +96,213 @@
   }
 // public end
 
+function loadData(){
+  // 本地
+  var getOption={
+    ajaxType:"GET", 
+    urlStr:"html/jsondata/pirates/ranking_datas.json",
+    ajaxData:null
+  };
+  // 測試
+  var postOption={
+    ajaxType:"POST", 
+    urlStr:"v2/activity/pirate/general_data",
+    ajaxData:{
+      "HTTP_USER_TOKEN":"9907fd6462e52fc064f24d0c947bbcd4",
+      "HTTP_USER_UID":1024317, 
+      "anchor_pfid":1000054
+    }
+  }
+  nativeAjax(postOption,function(data){
+    // 成功函数
+    console.log(data);
+    if(data.ret_code=="0"){
+      var alldata=data.data;
+      var arr=[];
+      arr.push(alldata.daily);
+      arr.push(alldata.all);
+      printTwo(arr);
+      // 貢獻榜
+      var contributions=alldata.send;
+      printCon(contributions);
+      addEvent();
+    }
+  },function(error){
+    // 失败返回HTTP状态码
+    console.log(error);
 
+  });
+}
+loadData();
 
+// 渲染日和總
+function printTwo(arr){
+  var len=arr.length;
+  var usersItem=document.querySelectorAll(".ranking-user-list");
+  var myItem=document.querySelectorAll(".anchor-self");
+  for(var i=0;i<len;i++){
+    // 日和總
+    var userLists=arr[i].list;
+    var innerLength=userLists.length;
+    var user_str="";
+    for(var j=0;j<innerLength;j++){
+      var nowList=userLists[j];
+      var live_id=nowList.live_id;
+      var img_src=nowList.headimg;
+      var live_key=nowList.live_key;
+      var live_url=nowList.live_url;
+      var nick_name=nowList.nickname;
+      var user_pfid=nowList.pfid;
+      var user_ranking=nowList.ranking;
+      user_ranking<10&&(user_ranking="0"+user_ranking);
+      var user_score=nowList.score;
+      var stream_direction=nowList.stream_direction;
+      var cdn_id=nowList.cdn_id;
 
+      if(pfid==user_pfid){
+        user_str+='<li class="myself">';
+      }else{
+        user_str+='<li>';
+      }
+      user_str+='<b>'+user_ranking+'</b>';
+      user_str+='<div class="ranking-user-outer">';
+      user_str+='<div class="ranking-user-img" data-cdnid="'+cdn_id+'" data-direction="'+stream_direction+'" data-name="'+nick_name+'" data-pfid="'+user_pfid+'" data-liveid="'+live_id+'" data-livekey="'+live_key+'" data-liveurl="'+live_url+'">';
+      user_str+='<img src="'+img_src+'" alt="">';
+      user_str+='</div>';
+      if(live_id!=null){
+        user_str+='<i></i>';
+      }
+      if(j==0){
+        user_str+='<span>傭兵之王</span>';
+      }else if(j==1){
+        user_str+='<span>傳奇傭兵</span>';
+      }else if(j==2){
+        user_str+='<span>赤日傭兵</span>';
+      }
+      user_str+='</div>';
+      user_str+='<p>'+nick_name+'</p>';
+      user_str+='<span>賞金'+user_score+'</span>';
+      user_str+='</li>';
+    }
+    // html
+    usersItem[i].innerHTML=user_str;
+    // 底部日和總個人
+    var myInfo=arr[i].my;
+    var myImg=myInfo.headimg;
+    var myName=myInfo.nickname;
+    var myRanking=myInfo.ranking;
+    var myScore=myInfo.score;
+    var my_str="";
+    my_str+='<div class="anchor-self-img">';
+    my_str+='<img src="'+myImg+'" alt="" >';
+    my_str+='</div>';
+    my_str+='<div class="anchor-self-msg">';
+    my_str+='<h3>'+myName+'</h3>';
+    if(i==0){
+      my_str+='<p>里程數：<span>'+myScore+'</span></p>';
+    }else if(i==1){
+      my_str+='<p>總里程數：<span>'+myScore+'</span></p>';
+    }
+    my_str+='</div>';
+    my_str+='<b>排名<span>'+myRanking+'</span></b>';
+    my_str+='<i></i></div>';
+    myItem[i].innerHTML=my_str;
+  }
+  // console.log(arr);
+}
+// 渲染貢獻榜單
+function printCon(con){
+  var lists=con.list;
+  var length=lists.length;
+  var con_str="";
+  for(var c=0;c<length;c++){
+    var nowCon=lists[c];
+    var live_id=nowCon.live_id;
+    var img_src=nowCon.headimg;
+    var live_key=nowCon.live_key;
+    var live_url=nowCon.live_url;
+    var nick_name=nowCon.nickname;
+    var user_pfid=nowCon.pfid;
+    var user_ranking=nowCon.ranking;
+    user_ranking<10&&(user_ranking="0"+user_ranking);
+    var user_score=nowCon.score;
+    var stream_direction=nowCon.stream_direction;
+    var cdn_id=nowCon.cdn_id;
+    if(pfid==user_pfid){
+      con_str+='<li class="myself">';
+    }else{
+      con_str+='<li>';
+    }
+    con_str+='<b>'+user_ranking+'</b>';
+    con_str+='<div class="ranking-user-outer">';
+    con_str+='<div class="ranking-user-img" data-cdnid="'+cdn_id+'" data-direction="'+stream_direction+'" data-name="'+nick_name+'" data-pfid="'+user_pfid+'" data-liveid="'+live_id+'" data-livekey="'+live_key+'" data-liveurl="'+live_url+'">';
+    con_str+='<img src="'+img_src+'" alt="">';
+    con_str+='</div>';
+    if(live_id!=null){
+      con_str+='<i></i>';
+    }
+    if(c==0){
+      con_str+='<span>海賊船長</span>';
+    }else if(c==1){
+      con_str+='<span>海賊大副</span>';
+    }else if(c==2){
+      con_str+='<span>海賊船員</span>';
+    }
+    con_str+='</div>';
+    con_str+='<div class="ranking-contribution-info">';
+    con_str+='<h3>'+nick_name+'</h3>';
+    con_str+='<p>貢獻里程：<span>'+user_score+'</span></p>';
+    con_str+='<i></i></div>';
+  }
+  // 追加HTML
+  pushHtml("rankCon",con_str);
+  // 貢獻自己
+  var conMe=con.my;
+  var myImg=conMe.headimg;
+  var myName=conMe.nickname;
+  var myRanking=conMe.ranking;
+  var myScore=conMe.score;
+  var conmy_str="";
+  conmy_str+='<div class="anchor-self-img">';
+  conmy_str+='<img src="'+myImg+'" alt="" >';
+  conmy_str+='</div>';
+  conmy_str+='<div class="anchor-self-msg">';
+  conmy_str+='<h3>'+myName+'</h3>';
+  conmy_str+='<p>貢獻<span>'+myScore+'</span></p>';
+  conmy_str+='</div>';
+  conmy_str+='<b>排名<span>'+myRanking+'</span></b>';
+  conmy_str+='</div>';
+  // 追加HTML
+  pushHtml("conMe",conmy_str);
+  // console.log(con);
+}
+// 點擊頭像跳轉
+function clickImg(){
+  var liveid=this.getAttribute("data-liveid");
+  var liveurl=this.getAttribute("data-liveurl");
+  var livekey=this.getAttribute("data-livekey");
+  var livepfid=this.getAttribute("data-pfid");
+  var nickname=this.getAttribute("data-name");
+  var direction=this.getAttribute("data-direction");
+  var cdn_id=this.getAttribute("data-cdnid");
+
+  if(liveid!="null"){
+    // 直播間
+    alert("跳轉直播間");
+    // h5toRoom(livepfid,nickname,liveid,liveurl,livekey,direction,cdn_id);
+  }else{
+    // 個人主頁
+    alert("個人主頁！");
+    // h5toHomepage(livepfid,nickname);
+  }
+}
+function addEvent(){
+  var btns=document.querySelectorAll(".ranking-user-img");
+  var btnLength=btns.length;
+  for(var m=0;m<btnLength;m++){
+    btns[m].addEventListener("click",clickImg);
+  }
+}
 // tab切換
 var btns=document.querySelectorAll(".ranking-tab-btn");
 var items=document.querySelectorAll(".ranking-lists-info");
@@ -129,4 +333,11 @@ for(var i=0;i<btns.length;i++){
     ruleBox.className="";
   }
   //
+  // 海賊榜單頁
+  var joinRank=document.getElementById("jumpTo");
+  var jumpRankForm=document.getElementById("jumpRank");
+  joinRank.onclick=function(){
+    jumpRankForm.submit();
+  }
+
 }();
