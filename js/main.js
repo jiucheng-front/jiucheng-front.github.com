da9989c87a0b0738107320d3978cff7a53c70fd2
wangjianfei <444128852@qq.com>
2017-09-29 18:56:44 +0800

    remove the CDN jQuery

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
  */
  require.config({
  	paths:{
- 		"jQuery":["https://cdn.bootcss.com/jquery/1.11.3/jquery.min","jquery-1.11.3"],
+ 		"jQuery":["jquery-2.2.3.min","jquery-1.11.3"],
  		"willRotate":"list/willRotate.min"
  	},
  	shim:{
