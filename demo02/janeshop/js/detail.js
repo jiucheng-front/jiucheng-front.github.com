507d46669e82ec002ccbaa92cef39b6ca96c4170
wangjianfei <444128852@qq.com>
2017-12-15 15:49:27 +0800

    replace theme to Color

--- demo02/janeshop/js/detail.js
+++ demo02/janeshop/js/detail.js
@@ -2,110 +2,41 @@ window.$=HTMLElement.prototype.$=function(selector){
     return (this==window?document:this).querySelectorAll(selector);
 }
 /*************更换主题*************/
-//获取每个li并绑定事件
-var li=$("#colors li");
-//console.log(li);
-li[0].onclick=changeClass0;
-li[1].onclick=changeClass1;
-li[2].onclick=changeClass2;
-li[3].onclick=changeClass3;
-li[4].onclick=changeClass4;
-li[5].onclick=changeClass5;
-var b=$('#colors li b');
-//获取每个li下的b元素，添加或者删除className来改变背景图片，注意：利用选择器的优先级
-//每个事件中同时改变网页内容中其他带背景色的位置
-//console.log(b);
-function changeClass0(){
-    b[0].className='selected';
-    b[1].className="";
-    b[2].className="";
-    b[3].className="";
-    b[4].className="";
-    b[5].className="";
-    $('.nav')[0].style.backgroundColor='#4261A4';
-    $('.left h5')[0].style.backgroundColor='#4261A4';
-    $('.left')[0].style.borderColor='#4261A4';
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#4261A4';
+var Color = {
+    colorBtns:$(".color"),
+    defaultClass:"color",
+    nav:$(".nav")[0],
+    leftBox:$(".left")[0],
+    leftTitle:$(".left h5")[0],
+    leftInnerTitle:$(".left h6"),
+    init(){
+        var length = this.colorBtns.length;
+        var _this = this;
+        for(var i = 0 ; i < length ; i++ ){
+            this.colorBtns[i].addEventListener("click",this.addSelected);
+        }
+    },
+    addSelected(){
+        if(this.className.indexOf("selected")==-1){
+            var color = this.getAttribute("data-color");
+            for(var i = 0; i < Color.colorBtns.length; i++){
+                Color.colorBtns[i].className = Color.defaultClass;
+            }
+            this.className = Color.defaultClass +" selected";
+            //切换主题颜色
+            Color.changeTheme(color);
+        }
+    },
+    changeTheme(color){
+        this.nav.style.backgroundColor=color;
+        this.leftBox.style.borderColor=color;
+        this.leftTitle.style.backgroundColor=color;
+        for(var j = 0; j < this.leftInnerTitle.length; j++){
+            this.leftInnerTitle[j].style.borderBottomColor=color;
+        }
     }
 }
-function changeClass1(){
-    b[0].className="";
-    b[1].className='selected';
-    b[2].className="";
-    b[3].className="";
-    b[4].className="";
-    b[5].className="";
-    $('.nav')[0].style.backgroundColor='#AFD400';
-    $('.left h5')[0].style.backgroundColor='#AFD400';
-    $('.left')[0].style.borderColor='#AFD400';
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#AFD400';
-    }
-}
-function changeClass2(){
-    b[0].className="";
-    b[1].className="";
-    b[2].className='selected';
-    b[3].className="";
-    b[4].className="";
-    b[5].className="";
-    $('.nav')[0].style.backgroundColor='#FBA90D';
-    $('.left h5')[0].style.backgroundColor='#FBA90D';
-    $('.left')[0].style.borderColor='#FBA90D';
-    //h6有四个
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#FBA90D';
-    }
-}
-function changeClass3(){
-    b[0].className="";
-    b[1].className="";
-    b[2].className="";
-    b[3].className='selected';
-    b[4].className="";
-    b[5].className="";
-    $('.nav')[0].style.backgroundColor='#27C2D1';
-    $('.left h5')[0].style.backgroundColor='#27C2D1';
-    $('.left')[0].style.borderColor='#27C2D1';
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#27C2D1';
-    }
-}
-function changeClass4(){
-    b[0].className="";
-    b[1].className="";
-    b[2].className="";
-    b[3].className="";
-    b[4].className='selected';
-    b[5].className="";
-    $('.nav')[0].style.backgroundColor='#E11556';
-    $('.left h5')[0].style.backgroundColor='#E11556';
-    $('.left')[0].style.borderColor='#E11556';
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#E11556';
-    }
-}function changeClass5(){
-    //b[0].className="";
-    //b[1].className="";
-    //b[2].className="";
-    //b[3].className="";
-    //b[4].className="";
-    b[0].className=b[1].className=b[2].className=b[3].className=b[4].className="";
-    b[5].className='selected';
-    $('.nav')[0].style.backgroundColor='#BA3AD9';
-    $('.left h5')[0].style.backgroundColor='#BA3AD9';
-    $('.left')[0].style.borderColor='#BA3AD9';
-    var h6=$('.left h6');
-    for(var i=0;i<h6.length;i++){
-        $('.left h6')[i].style.borderBottomColor='#BA3AD9';
-    }
-}
+
 /********主导航的下拉选项******/
 var li_lists=$('.nav ul li');
 //console.log(li_lists);
@@ -281,6 +212,9 @@ var Score={
 }
 
 window.onload=function(){
+    //1
+    Color.init();
+    //2
     Score.init();
 }
 
